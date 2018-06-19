47feb62dd917d2751d69fd3a59473d34845790e3
Alvaro Herrera <aherrerc@everis.com>
2018-06-19 14:37:56 -0500

    initial configuration

---
+++ Classes/CustomUIButton.swift
@@ -0,0 +1,20 @@
+//
+//  CustomUIButton.swift
+//  BCPSwiftButton
+//
+//  Created by Alvaro Herrera on 6/19/18.
+//
+
+import UIKit
+
+class CustomUIButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
