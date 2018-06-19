a71bbf23803639e3e1ed95083aac4cea4cc58149
Alvaro Herrera <aherrerc@everis.com>
2018-06-19 12:20:28 -0500

    Create example project via pod lib.

---
+++ Example/Pods/Target Support Files/BCPSwiftButton/BCPSwiftButton-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BCPSwiftButton : NSObject
+@end
+@implementation PodsDummy_BCPSwiftButton
+@end
---
+++ Example/Pods/Target Support Files/Pods-BCPSwiftButton_Tests/Pods-BCPSwiftButton_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BCPSwiftButton_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_BCPSwiftButton_Tests
+@end
