ba04c13b1c374c978aaf283584dfe9834ed946e2
Alvaro Herrera <aherrerc@everis.com>
2018-06-20 18:06:23 -0500

    custom button

--- Classes/CustomUIButton.swift
+++ Classes/CustomUIButton.swift
@@ -7,14 +7,39 @@
 
 import UIKit
 
-public class CustomUIButton: UIButton {
+// MARK: Protocol
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+@objc public protocol CustomUIButtonDelegate {
+    func didEndAnimation(button:UIButton)
+}
+
+@objcMembers public class CustomUIButton: UIButton {
+    
+    // MARK: Properties
+    
+    weak open var customDelegate:CustomUIButtonDelegate!
+    
+    // MARK: Overrides
+    
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        self.layer.cornerRadius = 10;
     }
-    */
-
+    
+    // MARK: Methods
+    
+    public func customAnimationForButton () -> Void {
+        self.performAnimation()
+    }
+    
+    func performAnimation () -> Void {
+        UIView.animate(withDuration: 1.0, animations: {
+            self.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+        },completion: {_ in UIView.animate(withDuration: 1.0) {
+            self.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            self.customDelegate.didEndAnimation(button: self)
+            }
+        })
+    }
+    
 }
